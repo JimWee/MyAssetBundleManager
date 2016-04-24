4fdfb7d34ddfa56059ae43ce1dae64e6e774c6d6
JianhuiWei <18817551783@163.com>
2016-04-24 16:52:32 +0800

    unitywebrequest

---
+++ Assets/Scripts/HttpDownloader/DownloadHandlerFile.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Networking;
+using System.Collections;
+using System.IO;
+
+
+namespace HttpDownloader
+{
+    class DownloadHandlerFile : DownloadHandlerScript
+    {
+        FileStream mFileStream;
+
+        public DownloadHandlerFile(string filePath, FileMode fileMode = FileMode.Create) 
+            : base()
+        {
+            mFileStream = new FileStream(filePath, fileMode);
+        }
+
+        public DownloadHandlerFile(byte[] buff, string filePath, FileMode fileMode = FileMode.Create) 
+            : base(buff)
+        {
+            mFileStream = new FileStream(filePath, fileMode);
+        }
+
+        protected override byte[] GetData()
+        {
+            return null;    
+        }
+
+        protected override string GetText()
+        {
+            return null;
+        }
+
+        protected override void ReceiveContentLength(int contentLength)
+        {
+            base.ReceiveContentLength(contentLength);
+            Debug.Log(string.Format("DownloadHandlerFiles :: ReceiveContentLength - length {0}", contentLength));
+        }
+
+        protected override bool ReceiveData(byte[] data, int dataLength)
+        {
+            if (data == null || data.Length < 1)
+            {
+                Debug.Log("DownloadHandlerFile :: ReceiveData - received a null/empty buffer");
+                return false;
+            }
+
+            Debug.Log(string.Format("DownloadHandlerFile :: ReceiveData - received {0} bytes", dataLength));
+            mFileStream.Write(data, 0, dataLength);
+            mFileStream.Flush();
+            return true;
+        }
+
+        protected override void CompleteContent()
+        {
+            Debug.Log("DownloadHandlerFile :: CompleteContent - DOWNLOAD COMPLETE!");
+            mFileStream.Close();
+        }
+    }
+}
---
+++ Assets/Scripts/HttpDownloader/Downloader.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.Experimental.Networking;
+using AssetBundles;
+using System.Net;
+
+namespace HttpDownloader
+{
+    public class Downloader : MonoBehaviour
+    {
+
+        public string Url = "http://192.168.1.109:7788/";
+        public string FileName = "1.zip";
+        byte[] mBuff = new byte[1024 * 1024];
+        UnityWebRequest mUwr;
+        bool mIsDownloading = false;
+
+        // Use this for initialization
+        void Start()
+        {
+            //StartCoroutine(GetTextWWW());
+            //StartCoroutine(GetText());
+            //GetTextHttp();
+            StartCoroutine(Download());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (mIsDownloading && mUwr != null)
+            {
+                Debug.Log(mUwr.downloadProgress);
+            }            
+        }
+
+        IEnumerator Download()
+        {
+            mUwr = new UnityWebRequest(Url + FileName, UnityWebRequest.kHttpVerbGET);
+            mUwr.downloadHandler = new DownloadHandlerFile(mBuff, Path.Combine(AssetBundleUtility.LocalAssetBundlePath, FileName));
+            mIsDownloading = true;
+            yield return mUwr.Send();
+            mIsDownloading = false;
+        }
+
+        IEnumerator GetText()
+        {
+            UnityWebRequest www = UnityWebRequest.Get(Url + FileName);
+            //Debug.Log(www.GetRequestHeader("Pragma"));
+            //www.SetRequestHeader("Range", "bytes=500-");
+            //Debug.Log(www.GetRequestHeader("Range"));
+            Debug.Log(Time.realtimeSinceStartup);
+            yield return www.Send();
+
+            if (www.isError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(Time.realtimeSinceStartup);
+                // Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+                File.WriteAllBytes(Path.Combine(AssetBundleUtility.LocalAssetBundlePath, FileName), results);
+            }
+        }
+
+        IEnumerator GetTextWWW()
+        {
+            WWW www = new WWW(Url + FileName);
+            yield return www;
+
+            if (!string.IsNullOrEmpty(www.error))
+            {
+                Debug.Log(www.error);
+            }
+            else if (www.isDone)
+            {
+
+                File.WriteAllBytes(Path.Combine(AssetBundleUtility.LocalAssetBundlePath, FileName), www.bytes);
+            }
+        }
+
+        void GetTextHttp()
+        {
+            HttpWebRequest www = (HttpWebRequest)HttpWebRequest.Create(Url + FileName);
+            www.Method = WebRequestMethods.Http.Post;
+            www.AddRange(200, 300);
+            using (WebResponse wr = www.GetResponse())
+            {
+                Stream inStream = wr.GetResponseStream();
+                FileStream outStream = new FileStream(Path.Combine(AssetBundleUtility.LocalAssetBundlePath, FileName), FileMode.Create, FileAccess.Write);
+                int count = 0;
+                int buffSize = 1024 * 1000;
+                byte[] buff = new byte[buffSize];
+                while ((count = inStream.Read(buff, 0, buffSize)) > 0)
+                {
+                    outStream.Write(buff, 0, count);
+                    outStream.Flush();
+                }
+                outStream.Close();
+            }
+        }
+    }
+}
